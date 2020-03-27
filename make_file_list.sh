105c86cbc2f1e928b4e71c13b4380b2768721eb3
yuangongji <yuangongji@foxmail.com>
2020-03-27 16:45:23 +0800

    eee

--- make_file_list.sh
+++ make_file_list.sh
@@ -22,6 +22,7 @@ done
 
 find src -name "*.c" | sed -e 's/\s\+/\n/g' | sort > srcs.tmp
 find include -name "*.h" | grep -vFf c_headers.tmp | sed -e 's/\s\+/\n/g' | sort > c_cpp_headers.tmp
+find include -name "*.hpp" | sed -e 's/\s\+/\n/g' | sort > cpp_headers.tmp
 
 echo 'LIST (APPEND msgpackc_SOURCES' > Files.cmake
 cat srcs.tmp | sed -e 's/^/    /g' >> Files.cmake
@@ -32,10 +33,16 @@ LIST (APPEND msgpackc_HEADERS' >> Files.cmake
 cat c_cpp_headers.tmp | sed -e 's/^/    /g' >> Files.cmake
 echo -e ')\n' >> Files.cmake
 
-echo '# Only for C library
+echo 'IF (NOT MSGPACK_CXX_ONLY)
+    # Only for C library
     LIST (APPEND msgpackc_HEADERS' >> Files.cmake
 cat c_headers.tmp | sed -e 's/^/        /g' >> Files.cmake
-echo -e '    )\n' >> Files.cmake
+echo -e '    )\nENDIF ()\n' >> Files.cmake
 
+echo 'IF (MSGPACK_ENABLE_CXX)
+    # Only for C++ library
+    LIST (APPEND msgpackc_HEADERS' >> Files.cmake
+cat cpp_headers.tmp | sed -e 's/^/        /g' >> Files.cmake
+echo -e '    )\nENDIF ()' >> Files.cmake
 
-rm -f srcs.tmp c_headers.tmp  c_cpp_headers.tmp
+rm -f srcs.tmp c_headers.tmp cpp_headers.tmp c_cpp_headers.tmp
