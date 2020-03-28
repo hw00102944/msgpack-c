4df1faa4eadf1aca71a887d714fbcb85a803bbd0
yuangongji <yuangongji@foxmail.com>
2020-03-28 09:09:28 +0800

    f

--- include/msgpack/sysdep.h
+++ include/msgpack/sysdep.h
@@ -93,6 +93,10 @@
 
 #endif
 
+#if !defined(MSGPACK_ENDIAN_LITTLE_BYTE) && !defined(MSGPACK_ENDIAN_BIG_BYTE)
+#include <msgpack/predef/other/endian.h>
+#endif // !defined(MSGPACK_ENDIAN_LITTLE_BYTE) && !defined(MSGPACK_ENDIAN_BIG_BYTE)
+
 #if MSGPACK_ENDIAN_LITTLE_BYTE
 
 #   if defined(unix) || defined(__unix) || defined(__APPLE__) || defined(__OpenBSD__)
